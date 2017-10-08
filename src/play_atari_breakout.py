727faa573b0cfc8f6f0ca7ded09155d5c9a357e0
skconan <supakit.kr@gmail.com>
2017-10-09 00:10:09 +0700

    pass test findBall()

--- src/play_atari_breakout.py
+++ src/play_atari_breakout.py
@@ -4,7 +4,7 @@ from PIL import ImageGrab
 import math
 from pynput.keyboard import Key
 from pynput.keyboard import Controller as keyboardCtrl
-
+from operator import itemgetter
 imgColor = None
 widthScreen = 1920
 heightScreen = 1080
@@ -35,11 +35,38 @@ def screenshotCapture():
         bbox=(int(widthScreen / 2), 240, widthScreen, heightScreen))
     return np.array(img)
 
+def circle_area(radius):
+    return math.pi * (radius**2)
 
-def findBall():
+def findBall(img):
     x, y = 0, 0
     lowerCir = np.array([30, 30, 30], np.uint8)
     upperCir = np.array([40, 40, 40], np.uint8)
+    
+    imgInRange = cv2.inRange(img,lowerCir,upperCir)
+    ret, th = cv2.threshold(imgInRange, 127, 255, 0)
+    imgCnts, cnts, _ = cv2.findContours(th, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    res = []
+    resImg = img*0
+    cv2.drawContours(resImg, cnts, -1, (0,255,0), 3)
+    
+    for c in cnts:
+        area = cv2.contourArea(c)
+        (x, y), r = cv2.minEnclosingCircle(c)
+        areaCir = circle_area(r)
+        if abs(area-areaCir) <= 40:
+            print(abs(area-areaCir))
+            cv2.circle(resImg, (int(x), int(y)), int(r), (255, 0, 0), -1)
+            res.append([x,y,r])
+    if len(res) > 0:
+        resSorted = sorted(res, key=itemgetter(-1))
+        resSorted = resSorted[-1]
+        x = resSorted[0]
+        y = resSorted[1]
+    cv2.imshow('resImgCir',resImg)  
+    key = cv2.waitKey(1) & 0xff
+    if key == ord('q'):
+        exit()
     return x, y
 
 
@@ -58,9 +85,9 @@ def main():
         if img is None:
             continue
         xCir, yCir = findBall(img)
-        xRect, yRect = findPlatform(img)
+        # xRect, yRect = findPlatform(img)
 
 
 if __name__ == '__main__':
     main()
-    getColor()
+    # getColor()
