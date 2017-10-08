b68e86746143cc2820b76553a7302eb931c7e578
skconan <supakit.kr@gmail.com>
2017-10-09 01:18:04 +0700

    fix getColor()

--- src/play_atari_breakout.py
+++ src/play_atari_breakout.py
@@ -12,8 +12,8 @@ heightScreen = 1080
 
 def getPosition(event, x, y, flags, param):
     global imgColor
-    if event == cv2.EVENT_LBUTTONDBLCLK and imgColor is not None:
-        print(imgColor[x, y])
+    if event == cv2.EVENT_MOUSEMOVE and imgColor is not None:
+        print(x,y,imgColor[y, x])
 
 
 def getColor():
@@ -22,6 +22,7 @@ def getColor():
     cv2.setMouseCallback('image', getPosition)
     while True:
         imgColor = screenshotCapture()
+        # imgColor = cv2.imread('C:/Users/skconan/Desktop/imgCtrlKeyboard/src/images/Screenshot (40).png',1)
         if imgColor is None:
             continue
         cv2.imshow('image', imgColor)
@@ -33,47 +34,78 @@ def getColor():
 def screenshotCapture():
     img = ImageGrab.grab(
         bbox=(int(widthScreen / 2), 240, widthScreen, heightScreen))
-    return np.array(img)
+    img = np.array(img)
+
+    return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
 
 def circle_area(radius):
     return math.pi * (radius**2)
 
+
 def findBall(img):
     x, y = 0, 0
-    lowerCir = np.array([30, 30, 30], np.uint8)
-    upperCir = np.array([40, 40, 40], np.uint8)
-    
-    imgInRange = cv2.inRange(img,lowerCir,upperCir)
+    lowerCir = np.array([250, 250, 250], np.uint8)
+    upperCir = np.array([255, 255, 40], np.uint8)
+
+    imgInRange = cv2.inRange(img, lowerCir, upperCir)
     ret, th = cv2.threshold(imgInRange, 127, 255, 0)
-    imgCnts, cnts, _ = cv2.findContours(th, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    imgCnts, cnts, _ = cv2.findContours(
+        th, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     res = []
-    resImg = img*0
-    cv2.drawContours(resImg, cnts, -1, (0,255,0), 3)
-    
+    resImg = img * 0
+    cv2.drawContours(resImg, cnts, -1, (0, 255, 0), 3)
+
     for c in cnts:
         area = cv2.contourArea(c)
         (x, y), r = cv2.minEnclosingCircle(c)
         areaCir = circle_area(r)
-        if abs(area-areaCir) <= 40:
-            print(abs(area-areaCir))
+        if abs(area - areaCir) <= 40:
+            print(abs(area - areaCir))
             cv2.circle(resImg, (int(x), int(y)), int(r), (255, 0, 0), -1)
-            res.append([x,y,r])
+            res.append([x, y, r])
     if len(res) > 0:
         resSorted = sorted(res, key=itemgetter(-1))
         resSorted = resSorted[-1]
         x = resSorted[0]
         y = resSorted[1]
-    cv2.imshow('resImgCir',resImg)  
+    cv2.imshow('resImgCir', resImg)
     key = cv2.waitKey(1) & 0xff
     if key == ord('q'):
         exit()
     return x, y
 
 
-def findPlatform():
+def findPlatform(img):
     x, y = 0, 0
-    lowerRect = np.array([30, 30, 30], np.uint8)
-    upperRect = np.array([40, 40, 40], np.uint8)
+    lowerRect = np.array([40,  69,  190], np.uint8)
+    upperRect = np.array([50,  70,  200], np.uint8)
+
+    imgInRange = cv2.inRange(img, lowerRect, upperRect)
+    ret, th = cv2.threshold(imgInRange, 127, 255, 0)
+    imgCnts, cnts, _ = cv2.findContours(
+        th, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    res = []
+    resImg = img * 0
+    cv2.drawContours(resImg, cnts, -1, (0, 255, 0), 3)
+
+    for c in cnts:
+        areaCnt = cv2.contourArea(c)
+        x, y, w, h = cv2.boundingRect(c)
+        areaRect = w * h
+        if areaCnt / areaRect >= 0.8 and w / (h * 1.0) >= 10:
+            cv2.rectangle(resImg, (x, y), (x + w, y + h), (255, 255, 0), -1)
+            res.append((areaCnt, x, y, w, h))
+
+    if len(res) > 0:
+        resSorted = sorted(res, key=itemgetter(0))
+        resSorted = resSorted[0]
+        x = resSorted[1]
+        y = resSorted[2]
+    cv2.imshow('resImgRect', resImg)
+    key = cv2.waitKey(1) & 0xff
+    if key == ord('q'):
+        exit()
     return x, y
 
 
@@ -84,10 +116,10 @@ def main():
         img = screenshotCapture()
         if img is None:
             continue
-        xCir, yCir = findBall(img)
-        # xRect, yRect = findPlatform(img)
+        # xCir, yCir = findBall(img)
+        xRect, yRect = findPlatform(img)
 
 
 if __name__ == '__main__':
-    main()
-    # getColor()
+    # main()
+    getColor()
