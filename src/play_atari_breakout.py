ed33c6f4170c802a093a8d03bf663bb4d7ddfcff
skconan <supakit.kr@gmail.com>
2017-10-09 15:54:06 +0700

    refactor code

--- src/play_atari_breakout.py
+++ src/play_atari_breakout.py
@@ -11,27 +11,14 @@ widthScreen = 1920
 heightScreen = 1080
 
 
-def getPosition(event, x, y, flags, param):
+def get_position(event, x, y, flags, param):
     global imgColor
     if event == cv2.EVENT_MOUSEMOVE and imgColor is not None:
         print(x, y, imgColor[y, x])
 
 
-def getColor():
-    global imgColor
-    cv2.namedWindow('image')
-    cv2.setMouseCallback('image', getPosition)
-    while True:
-        imgColor = screenshotCapture()
-        if imgColor is None:
-            continue
-        cv2.imshow('image', imgColor)
-        key = cv2.waitKey(1) & 0xff
-        if key == ord('q'):
-            break
 
-
-def screenshotCapture():
+def screenshot_capture():
     img = pyautogui.screenshot(
         region=(int(widthScreen / 2), 240, int(widthScreen / 2), heightScreen - 240))
     img = np.array(img)
@@ -43,7 +30,7 @@ def circle_area(radius):
     return math.pi * (radius**2)
 
 
-def findBall(img):
+def find_ball(img):
     global resImg
     x, y = 0, 0
     lowerCir = np.array([30, 30, 30], np.uint8)
@@ -71,7 +58,7 @@ def findBall(img):
     return x, y
 
 
-def findPlatform(img):
+def find_platform(img):
     global resImg
     x, y = 0, 0
     lowerRect = np.array([250,  130,  0], np.uint8)
@@ -129,12 +116,12 @@ def main():
         time.sleep(1)
 
     while True:
-        img = screenshotCapture()
+        img = screenshot_capture()
         if img is None:
             continue
         resImg = img * 0
-        xCir, yCir = findBall(img)
-        xRect, yRect, xL, xR = findPlatform(img)
+        xCir, yCir = find_ball(img)
+        xRect, yRect, xL, xR = find_platform(img)
         print(xCir, xRect, xCir - xRect)
         keyboard(xCir, xR, xL)
         r, c, ch = resImg.shape
